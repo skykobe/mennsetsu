04d51bf851f103672d79098a4a08659c0f505da7
Ricardo <lacushui123@163.com>
2018-06-26 14:08:30 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 ec.createComponent('page1', {
-  templateUrl: '/template/page1.html'
+  templateUrl: 'template/page1.html'
 })
 ec.createComponent('page2', {
-  templateUrl: '/template/page2.html'
+  templateUrl: 'template/page2.html'
 })
 ec.createComponent('page3', {
-  templateUrl: '/template/page3.html'
+  templateUrl: 'template/page3.html'
 })
 ec.createComponent('page4', {
-  templateUrl: '/template/page4.html'
+  templateUrl: 'template/page4.html'
 })
 ec.createComponent('page5', {
-  templateUrl: '/template/page5.html'
+  templateUrl: 'template/page5.html'
 })
 ec.createComponent('page6', {
-  templateUrl: '/template/page6.html'
+  templateUrl: 'template/page6.html'
+})
+ec.createComponent('page7', {
+  templateUrl: 'template/page7.html'
 })
 ec.componentIn(function() {
   ec.jsLoad('lib/impress.js', function(o) {
